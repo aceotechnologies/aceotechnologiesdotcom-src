536a7f9d7837b88bb92d076fc17da6840c4f4089
Ikennaf1 <iiikbros202@gmail.com>
2025-02-11 22:31:17 +0100

    Initialized base project

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig ({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                // about: resolve(__dirname, 'about/index.html'),
+                // what_we_do: resolve(__dirname, 'what-we-do/index.html'),
+                // why_beitor: resolve(__dirname, 'why-beitor/index.html'),
+                // contact_us: resolve(__dirname, 'contact-us/index.html'),
+            },
+        },
+    },
+    plugins: [
+        tailwindcss(),
+    ],
+})
