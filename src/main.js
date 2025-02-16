49078779fb37bf4260fa3718e5d4dad28d3d1e2b
Ikennaf1 <iiikbros202@gmail.com>
2025-02-16 12:06:36 +0100

    Header scaffold done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 // import './style.css'
+
+var navBtn = document.querySelector('#nav-open');
+var menu = document.querySelector('#nav-opened');
+var closeNav = document.querySelector('#close_nav');
+navBtn.onclick = () => {
+  menu.hidden = !menu.hidden;
+  navBtn.ariaHidden = !navBtn.ariaHidden;
+  navBtn.ariaExpanded = !navBtn.ariaExpanded;
+}
+
+closeNav.onclick = () => {
+    menu.hidden = !menu.hidden;
+    navBtn.ariaHidden = !navBtn.ariaHidden;
+    navBtn.ariaExpanded = !navBtn.ariaExpanded;
+}
